15c83f64d47f5cb6c38e6cf5709fb8be6566df88
Kirill <pritula_2005@mail.ru>
2026-08-31 23:28:03 +0500

    feat: added store Url in database

--- app/Http/Controllers/Url/UrlController.php
+++ app/Http/Controllers/Url/UrlController.php
@@ -7,9 +7,28 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests\Url\StoreRequest;
 
+use Illuminate\Support\Str;
+
+use App\Models\Url;
+
 class UrlController extends Controller
 {
     public function store(StoreRequest $request)
     {
+        $data = $request->validated();
+        $linkSource = $request->input('url');
+        $linkShorted = url('') . '/tl/' . Str::random(10);
+
+        Url::create([
+            'link_source' => $linkSource,
+            'link_shorted' => $linkShorted
+        ]);
+
+        return redirect()->back();
     }
+
+//    public function redirect($hashLink)
+//    {
+//
+//    }
 }
