0d518d03ba2cbd3d6df7dc4f173ad63f445b4afc
Kirill <pritula_2005@mail.ru>
2026-09-02 00:20:58 +0500

    feat: added router for show page current url in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\VueRouter\VueRouterController::class, 'index']);
-Route::get('/url', [\App\Http\Controllers\VueRouter\VueRouterController::class, 'index']);
+Route::get('/url/{url}', [\App\Http\Controllers\VueRouter\VueRouterController::class, 'index']);
 
 Route::get('/tl/{hashLink}', [\App\Http\Controllers\Url\UrlController::class, 'redirect'])
 ->where('hashLink', '[A-Za-z0-9]+');
