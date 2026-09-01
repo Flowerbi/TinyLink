9af861dca5bf4d06abc33fd3b1bb61426e30ba58
Kirill <pritula_2005@mail.ru>
2026-09-01 22:48:49 +0500

    build: setup vueRouter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
 import {createApp} from "vue";
 import App from "@/App.vue";
 import axios from "axios";
+import router from "@/router/router.js";
 
 const app = createApp(App);
-
+app.use(router);
 app.mount('#app');
---
+++ resources/js/router/router.js
@@ -0,0 +1,22 @@
+import {createRouter, createWebHistory} from "vue-router";
+import MainUrl from "@/Pages/MainUrl.vue";
+import CurrentUrl from "@/Pages/CurrentUrl.vue";
+const routes = [
+    {
+        path: '/',
+        component: MainUrl,
+        name: 'main-url.page'
+    },
+    {
+        path: '/url',
+        component: CurrentUrl,
+        name: 'current-url.page'
+    }
+];
+
+const router = createRouter({
+   history: createWebHistory(),
+   routes
+});
+
+export default router
