f0b75c8e6a169bef3f1173832bd90b8756f7df56
Kirill <pritula_2005@mail.ru>
2026-09-01 22:50:48 +0500

    feat: added routes for mainUrl and CurrentUrl pages

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\MainPage\MainPageController::class, 'index'])->name('main-page.index');
+Route::get('/', [\App\Http\Controllers\VueRouter\VueRouterController::class, 'index']);
+Route::get('/url', [\App\Http\Controllers\VueRouter\VueRouterController::class, 'index']);
 
 Route::get('/tl/{hashLink}', [\App\Http\Controllers\Url\UrlController::class, 'redirect'])
 ->where('hashLink', '[A-Za-z0-9]+');
